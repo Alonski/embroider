1050157578a0e9a00338a08b0367f3c241c284ef
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2023-11-13 17:46:06 +0300

    reverse exports: prototype implementation

--- packages/reverse-exports/src/index.ts
+++ packages/reverse-exports/src/index.ts
@@ -1,14 +1,118 @@
 import { posix } from 'path';
+import minimatch from 'minimatch';
+import { exports as resolveExports } from 'resolve.exports';
 
-export default function reversePackageExports(
-  packageJSON: { exports?: any; name: string },
-  relativePath: string
-): string {
-  // TODO add an actual matching system and don't just look for the default
-  if (packageJSON.exports?.['./*'] === './dist/*.js') {
-    return posix.join(packageJSON.name, relativePath.replace(/^.\/dist\//, `./`).replace(/\.js$/, ''));
+type Exports = string | string[] | { [key: string]: Exports };
+
+/**
+ * An util to find a string value in a nested JSON-like structure.
+ *
+ * Receives an object (a netsted JSON-like structure) and a matcher callback
+ * that is tested against each string value.
+ *
+ * When a value is found, returns an object containing a `value` and a `key`.
+ * The key is one of the parent keys of the found value — the one that starts
+ * with `.`.
+ *
+ * When a value is not found, returns `undefined`.
+ */
+export function _findPathRecursively(
+  exportsObj: Exports,
+  matcher: (path: string) => boolean,
+  key = '.'
+): { key: string; value: Exports } | undefined {
+  if (typeof exportsObj === 'string') {
+    return matcher(exportsObj) ? { key, value: exportsObj } : undefined;
   }
 
-  // TODO figure out what the result should be if it doesn't match anything in exports
-  return posix.join(packageJSON.name, relativePath);
+  if (Array.isArray(exportsObj)) {
+    const value = exportsObj.find(path => matcher(path));
+
+    if (value) {
+      return { key, value };
+    } else {
+      return undefined;
+    }
+  }
+
+  if (typeof exportsObj === 'object') {
+    let result: { key: string; value: Exports } | undefined = undefined;
+
+    for (const candidateKey in exportsObj) {
+      if (!exportsObj.hasOwnProperty(candidateKey)) {
+        return;
+      }
+
+      const candidate = _findPathRecursively(exportsObj[candidateKey], matcher, key);
+
+      if (candidate) {
+        result = {
+          key: candidateKey,
+          value: candidate.value,
+        };
+
+        break;
+      }
+    }
+
+    if (result) {
+      if (result.key.startsWith('./')) {
+        if (key !== '.') {
+          throw new Error(`exportsObj contains doubly nested path keys: "${key}" and "${result.key}"`);
+        }
+
+        return { key: result.key, value: result.value };
+      } else {
+        return { key, value: result.value };
+      }
+    } else {
+      return undefined;
+    }
+  }
+
+  throw new Error(`Unexpected type of obj: ${typeof exportsObj}`);
+}
+
+export default function reversePackageExports(
+  { exports: exportsObj, name }: { exports?: Exports; name: string },
+  relativePath: string
+): string {
+  // // TODO add an actual matching system and don't just look for the default
+  // if (packageJSON.exports?.['./*'] === './dist/*.js') {
+  //   return posix.join(packageJSON.name, relativePath.replace(/^.\/dist\//, `./`).replace(/\.js$/, ''));
+  // }
+
+  if (!exportsObj) {
+    return posix.join(name, relativePath);
+  }
+
+  const maybeKeyValuePair = _findPathRecursively(exportsObj, candidate => {
+    // miminatch does not treat directories as full of content without glob
+    if (candidate.endsWith('/')) {
+      candidate += '**';
+    }
+
+    return minimatch(relativePath, candidate);
+  });
+
+  if (!maybeKeyValuePair) {
+    // TODO figure out what the result should be if it doesn't match anything in exports
+    return posix.join(name, relativePath);
+  }
+
+  const { key, value } = maybeKeyValuePair;
+
+  if (typeof value !== 'string') {
+    throw new Error('Expected value to be a string');
+  }
+
+  const maybeResolvedPaths = resolveExports({ name, exports: { [value]: key } }, relativePath);
+
+  if (!maybeResolvedPaths) {
+    throw new Error('Expected path to be found at this point');
+  }
+
+  const [resolvedPath] = maybeResolvedPaths;
+
+  return resolvedPath.replace(/^./, name);
 }
--- packages/reverse-exports/tests/reverse-exports.test.ts
+++ packages/reverse-exports/tests/reverse-exports.test.ts
@@ -1,25 +1,23 @@
-import reversePackageExports from '../src';
+import reversePackageExports, { _findPathRecursively } from '../src';
 
 describe('reverse exports', function () {
-  it('correctly reversed exports', function () {
-    // TODO figure out what the result should be if it doesn't match anything in exports
-    expect(reversePackageExports({ name: 'best-addon' }, './dist/_app_/components/face.js')).toBe(
-      'best-addon/dist/_app_/components/face.js'
-    );
-
-    expect(
-      reversePackageExports(
-        {
-          name: 'best-addon',
-          exports: {
-            './*': './dist/*.js',
-          },
-        },
-        './dist/_app_/components/face.js'
-      )
-    ).toBe('best-addon/_app_/components/face');
-  });
-
+  //   it('correctly reversed exports', function () {
+  //     // TODO figure out what the result should be if it doesn't match anything in exports
+  //     expect(reversePackageExports({ name: 'best-addon' }, './dist/_app_/components/face.js')).toBe(
+  //       'best-addon/dist/_app_/components/face.js'
+  //     );
+  //     expect(
+  //       reversePackageExports(
+  //         {
+  //           name: 'best-addon',
+  //           exports: {
+  //             './*': './dist/*.js',
+  //           },
+  //         },
+  //         './dist/_app_/components/face.js'
+  //       )
+  //     ).toBe('best-addon/_app_/components/face');
+  //   });
   it('exports is a string', function () {
     const actual = reversePackageExports(
       {
@@ -28,10 +26,8 @@ describe('reverse exports', function () {
       },
       './foo.js'
     );
-
     expect(actual).toBe('my-addon');
   });
-
   it('exports is an object with one entry', function () {
     const actual = reversePackageExports(
       {
@@ -42,10 +38,8 @@ describe('reverse exports', function () {
       },
       './foo.js'
     );
-
     expect(actual).toBe('my-addon');
   });
-
   it('subpath exports', function () {
     const packageJson = {
       name: 'my-addon',
@@ -58,17 +52,13 @@ describe('reverse exports', function () {
         './glob/*': './grod/**/*.js',
       },
     };
-
     expect(reversePackageExports(packageJson, './main.js')).toBe('my-addon');
     expect(reversePackageExports(packageJson, './secondary.js')).toBe('my-addon/sub/path');
     expect(reversePackageExports(packageJson, './directory/some/file.js')).toBe('my-addon/prefix/some/file.js');
-    expect(reversePackageExports(packageJson, './other-directory/file.js')).toBe('addon/prefix/deep/file.js');
-
-    expect(reversePackageExports(packageJson, './yet-another/deep/file.js')).toBe('addon/other-prefix/deep/file');
-
-    expect(reversePackageExports(packageJson, './grod/very/deep/file.js')).toBe('addon/glob/very/deep/file');
+    expect(reversePackageExports(packageJson, './other-directory/file.js')).toBe('my-addon/prefix/deep/file.js');
+    expect(reversePackageExports(packageJson, './yet-another/deep/file.js')).toBe('my-addon/other-prefix/deep/file');
+    expect(reversePackageExports(packageJson, './grod/very/deep/file.js')).toBe('my-addon/glob/very/deep/file');
   });
-
   it('alternative exports', function () {
     const packageJson = {
       name: 'my-addon',
@@ -76,11 +66,9 @@ describe('reverse exports', function () {
         './things/': ['./good-things/', './bad-things/'],
       },
     };
-
     expect(reversePackageExports(packageJson, './good-things/apple.js')).toBe('my-addon/things/apple.js');
     expect(reversePackageExports(packageJson, './bad-things/apple.js')).toBe('my-addon/things/apple.js');
   });
-
   it('conditional exports - simple abbreviated', function () {
     const packageJson = {
       name: 'my-addon',
@@ -90,12 +78,10 @@ describe('reverse exports', function () {
         default: './index.js',
       },
     };
-
     expect(reversePackageExports(packageJson, './index-module.js')).toBe('my-addon');
     expect(reversePackageExports(packageJson, './index-require.cjs')).toBe('my-addon');
     expect(reversePackageExports(packageJson, './index.js')).toBe('my-addon');
   });
-
   it('conditional exports - simple non-abbreviated', function () {
     const packageJson = {
       name: 'my-addon',
@@ -107,29 +93,25 @@ describe('reverse exports', function () {
         },
       },
     };
-
     expect(reversePackageExports(packageJson, './index-module.js')).toBe('my-addon');
     expect(reversePackageExports(packageJson, './index-require.cjs')).toBe('my-addon');
     expect(reversePackageExports(packageJson, './index.js')).toBe('my-addon');
   });
-
   it('conditional subpath exports', function () {
     const packageJson = {
       name: 'my-addon',
       exports: {
         '.': './index.js',
         './feature.js': {
-          node: './feature-node.js',
+          node: './feature-node.cjs',
           default: './feature.js',
         },
       },
     };
-
     expect(reversePackageExports(packageJson, './index.js')).toBe('my-addon');
     expect(reversePackageExports(packageJson, './feature-node.cjs')).toBe('my-addon/feature.js');
     expect(reversePackageExports(packageJson, './feature.js')).toBe('my-addon/feature.js');
   });
-
   it('nested conditional exports', function () {
     const packageJson = {
       name: 'my-addon',
@@ -141,9 +123,129 @@ describe('reverse exports', function () {
         default: './feature.mjs',
       },
     };
-
     expect(reversePackageExports(packageJson, './feature-node.mjs')).toBe('my-addon');
     expect(reversePackageExports(packageJson, './feature-node.cjs')).toBe('my-addon');
     expect(reversePackageExports(packageJson, './feature.mjs')).toBe('my-addon');
+  });
+});
+
+/* 8888888888888888888888888888 */
+
+describe('_findKeyRecursively', function () {
+  it('Returns "." when string is provided and matcher is matching', function () {
+    expect(_findPathRecursively('foo', str => str === 'foo')).toStrictEqual({ key: '.', value: 'foo' });
+  });
+
+  it('Returns undefined when string is provided and matcher is not matching', function () {
+    expect(_findPathRecursively('foo', str => str === 'bar')).toBe(undefined);
+  });
+
+  it('Returns "." when array is provided and matcher is matching', function () {
+    expect(_findPathRecursively(['foo', 'bar'], str => str === 'bar')).toStrictEqual({ key: '.', value: 'bar' });
+  });
+
+  it('Returns undefined when array is provided and matcher is not matching', function () {
+    expect(_findPathRecursively(['foo', 'bar'], str => str === 'baz')).toBe(undefined);
+  });
+
+  it('Returns a matching key when a record of valid paths is provided and matcher is matching', function () {
+    const exports = {
+      '.': './main.js',
+      './sub/path': './secondary.js',
+      './prefix/': './directory/',
+      './prefix/deep/': './other-directory/',
+      './other-prefix/*': './yet-another/*/*.js',
+      './glob/*': './grod/**/*.js',
+    };
+
+    expect(_findPathRecursively(exports, str => str === './secondary.js')).toStrictEqual({
+      key: './sub/path',
+      value: './secondary.js',
+    });
+  });
+
+  it('Returns undefined when a record of valid paths is provided and matcher is not matching', function () {
+    const exports = {
+      '.': './main.js',
+      './sub/path': './secondary.js',
+      './prefix/': './directory/',
+      './prefix/deep/': './other-directory/',
+      './other-prefix/*': './yet-another/*/*.js',
+      './glob/*': './grod/**/*.js',
+    };
+
+    expect(_findPathRecursively(exports, str => str === './non-existent-path')).toBe(undefined);
+  });
+
+  it('Returns a matching key when a record of arrays is provided and matcher is matching', function () {
+    const exports = {
+      './foo': ['./bar', './baz'],
+      './zomg': ['./lol', './wtf'],
+    };
+
+    expect(_findPathRecursively(exports, str => str === './lol')).toStrictEqual({ key: './zomg', value: './lol' });
+  });
+
+  it('Returns undefined when a record of arrays is provided and matcher is not matching', function () {
+    const exports = {
+      './foo': ['./bar', './baz'],
+      './zomg': ['./lol', './wtf'],
+    };
+
+    expect(_findPathRecursively(exports, str => str === './rofl')).toBe(undefined);
+  });
+
+  it('Returns a matching key when a record of conditions with paths is provided and matcher is matching', function () {
+    const exports = {
+      '.': './index.js',
+      './feature.js': {
+        node: './feature-node.js',
+        default: './feature.js',
+      },
+    };
+
+    expect(_findPathRecursively(exports, str => str === './feature-node.js')).toStrictEqual({
+      key: './feature.js',
+      value: './feature-node.js',
+    });
+  });
+
+  it('Returns undefined when a record of conditions with paths is provided and matcher is not matching', function () {
+    const exports = {
+      '.': './index.js',
+      './feature.js': {
+        node: './feature-node.js',
+        default: './feature.js',
+      },
+    };
+
+    expect(_findPathRecursively(exports, str => str === './missing-path.js')).toBe(undefined);
+  });
+
+  it('Returns a matching key when a record of conditions withithout paths is provided and matcher is matching', function () {
+    const exports = {
+      node: {
+        import: './feature-node.mjs',
+        require: './feature-node.cjs',
+      },
+      default: './feature.mjs',
+    };
+
+    expect(_findPathRecursively(exports, str => str === './feature-node.cjs')).toStrictEqual({
+      key: '.',
+      value: './feature-node.cjs',
+    });
+  });
+
+  it('Returns undefined when a record of conditions without paths is provided and matcher is not matching', function () {
+    const exports = {
+      node: {
+        import: './feature-node.mjs',
+        require: './feature-node.cjs',
+      },
+      default: './feature.mjs',
+    };
+
+    expect(_findPathRecursively(exports, str => str === './missing-path.js')).toBe(undefined);
   });
 });
